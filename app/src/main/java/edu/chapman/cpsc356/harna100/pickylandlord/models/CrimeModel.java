944ebdfa9f856fcd262d5d54af4a809fe03b3605
Paul Harnack <harna100@mail.chapman.edu>
2017-10-23 18:29:53 -0700

    Adds DateTime to CrimeModel

--- app/src/main/java/edu/chapman/cpsc356/harna100/pickylandlord/models/CrimeModel.java
+++ app/src/main/java/edu/chapman/cpsc356/harna100/pickylandlord/models/CrimeModel.java
@@ -1,15 +1,19 @@
 package edu.chapman.cpsc356.harna100.pickylandlord.models;
 
+import org.joda.time.DateTime;
+
 import java.util.UUID;
 
 public class CrimeModel {
 	private String title;
 	private boolean solved;
 	private String id;
+	private DateTime creationDate;
 
 
 	public CrimeModel() {
 		this.id = UUID.randomUUID().toString();
+		this.creationDate = DateTime.now();
 	}
 
 	public String getId() {
@@ -30,5 +34,9 @@ public class CrimeModel {
 
 	public void setSolved(boolean solved) {
 		this.solved = solved;
+	}
+
+	public DateTime getCreationDate() {
+		return creationDate;
 	}
 }
