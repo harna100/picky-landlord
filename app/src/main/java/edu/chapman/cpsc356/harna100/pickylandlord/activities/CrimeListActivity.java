e6bf2dc57a2f84ff478c5945f63a24ded254c526
Paul Harnack <harna100@mail.chapman.edu>
2017-11-13 18:10:40 -0800

    Adds a basic skeleton for custom menu options

--- app/src/main/java/edu/chapman/cpsc356/harna100/pickylandlord/activities/CrimeListActivity.java
+++ app/src/main/java/edu/chapman/cpsc356/harna100/pickylandlord/activities/CrimeListActivity.java
@@ -1,7 +1,13 @@
 package edu.chapman.cpsc356.harna100.pickylandlord.activities;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import edu.chapman.cpsc356.harna100.pickylandlord.R;
 import edu.chapman.cpsc356.harna100.pickylandlord.fragments.CrimeListFragment;
 
 /**
@@ -12,5 +18,22 @@ public class CrimeListActivity extends SingleFragmentActivity {
 	@Override
 	protected Fragment getFragment() {
 		return CrimeListFragment.NewInstance();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.crime_list, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.menu_add_crime:
+				return true;
+			default:
+				return false;
+		}
 	}
 }
