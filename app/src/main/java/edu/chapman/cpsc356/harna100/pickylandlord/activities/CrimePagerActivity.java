f5b0b1e434947832f5f922ac800b75ee034b7172
Paul Harnack <harna100@mail.chapman.edu>
2017-11-15 17:50:06 -0800

    Creates ViewPager activity

---
+++ app/src/main/java/edu/chapman/cpsc356/harna100/pickylandlord/activities/CrimePagerActivity.java
@@ -0,0 +1,15 @@
+package edu.chapman.cpsc356.harna100.pickylandlord.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.chapman.cpsc356.harna100.pickylandlord.R;
+
+public class CrimePagerActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_crime_pager);
+	}
+}
